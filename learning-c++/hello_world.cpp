8f2f8b0e29f216e900dd8a1266b4bc1f89ed3ebb
francuzzito <56544800+francuzzito@users.noreply.github.com>
2025-07-09 22:49:27 +0200

    Add project files.

---
+++ learning-c++/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+// this is a simple C++ program that prints "hello world!" to the console.
+int main() {
+	// generate the message.
+	const std::string message = "hello world!";
+
+	// print the message to the console.
+	std::cout << message << "\n";
+
+	return 0;
+}
---
+++ learning-c++/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+// ask the user for a number and print its multiplication table up to 10.
+int main() {
+	// predefined variables.
+	int number;
+	std::cout << "enter a number: ";
+	std::cin >> number;
+
+	// print the multiplication table for the number.
+	for (int i = 1; i <= 10; ++i) {
+		std::cout << number << " * " << i << " = " << number * i << std::endl;
+	}
+
+	return 0;
+}
