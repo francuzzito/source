78d701d54e345b6adbe594e160085a097f32f9a6
francuzzito <56544800+francuzzito@users.noreply.github.com>
2025-07-09 23:00:40 +0200

    simple loop

---
+++ learning-c++/simple_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// print numbers from 1 to 10 using a loop.
+int main() {
+	// predefined variables.
+	int number = 0;
+
+	while (number <= 10) {
+		std::cout << number << "\n";
+		number++; // increment by one.
+	}
+
+	return 0;
+}
